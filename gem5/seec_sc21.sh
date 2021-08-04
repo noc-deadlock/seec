04a67c865e29c3ff3eb5bb55cecc11e85425fe7b
Mayank Parasar <mparasar3@gatech.edu>
2021-08-04 19:16:48 -0400

    mods to the bash script

--- gem5/seec_sc21.sh
+++ gem5/seec_sc21.sh
@@ -5,8 +5,8 @@
 # bench_caps=( 'BIT_COMPLEMENT' 'BIT_ROTATION' 'SHUFFLE' 'TRANSPOSE' 'UNIFORM_RANDOM' )
 # bench=( 'bit_complement' 'bit_rotation' 'shuffle' 'transpose' 'uniform_random' )
 
-bench_caps=( 'BIT_ROTATION' 'SHUFFLE' 'TRANSPOSE' 'UNIFORM_RANDOM')
-bench=( 'bit_rotation' 'shuffle' 'transpose' 'uniform_random')
+bench_caps=( 'BIT_ROTATION' 'SHUFFLE' 'TRANSPOSE')
+bench=( 'bit_rotation' 'shuffle' 'transpose')
 
 # bench_caps=( 'UNIFORM_RANDOM' 'BIT_COMPLEMENT' 'BIT_ROTATION' 'TRANSPOSE' 'SHUFFLE' )
 # bench=( 'uniform_random' 'bit_complement' 'bit_rotation' 'transpose' 'shuffle' )
@@ -15,7 +15,10 @@ routing_algorithm=( 'TABLE' 'XY' 'RANDOM' 'ADAPT_RAND' 'WestFirst' 'ESCAPE_VC' )
 
 d="11-24-2020"
 # out_dir="/usr/scratch/mayank/thesis/SEEC/$d"
-out_dir="/usr/scratch/mayank/seec_isca2021_rslt/$d"
+# out_dir="/usr/scratch/mayank/seec_isca2021_rslt/$d"
+rm -rf results
+mkdir results
+out_dir="results"
 cycles=100000
 vnet=0 #for multi-flit pkt: vnet = 2
 tr=1
@@ -26,12 +29,13 @@ bufferless_router=$3
 # for routAlgo in 1 2 3 4 5;
 # do
 	# for b in 0 1 2
-	for b in 0 1 2 3 4
+	for b in 0 1 2
 	do
-		for vc_ in 1
+		for vc_ in 4
 		# for vc_ in 8 12
 		do
-			for k in 0.02 0.04 0.06 0.08 0.10 0.12 0.14 0.16 0.18 0.20 0.22 0.24 0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.44 0.46 0.48 0.50 0.52 0.54 0.56 0.58 0.60 0.62 0.64 0.66 0.68 0.70
+			for k in 0.02 0.04 0.06 0.08 0.10 0.12 0.14 0.16 0.18 0.20 0.22 0.24 0.26 0.28 0.30 0.32 0.34 0.36 0.38 0.40 0.42 0.44 0.46 0.48 0.50
+			# 0.52 0.54 0.56 0.58 0.60 0.62 0.64 0.66 0.68 0.70
 			# 0.70 0.72 0.74 0.76 0.78 0.80 0.82 0.84 0.86 0.88 0.90 0.92 0.94 0.96 0.98 1.0
 			do
 				#################### 4x4 Mesh (multi-SEEC) ###########################
